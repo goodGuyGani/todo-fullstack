63a0b79ffd9cd92e3beaf4590fbeeaf400c4c5ee
goodGuyGani <harzhedzmig@gmail.com>
2023-12-25 02:58:16 +0800

    NavBar improved

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -8,8 +8,8 @@ function NavBar() {
     <nav className="flex w-full items-center justify-between p-4  px-8 h-[60px]">
       <Logo />
       <div className="flex gap-4 items-center">
-        <UserButton afterSignOutUrl="/" />
         <ThemeSwitcher />
+        <UserButton afterSignOutUrl="/" />
       </div>
     </nav>
   );
